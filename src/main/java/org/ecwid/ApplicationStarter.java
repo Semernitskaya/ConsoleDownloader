c5dbcd666d6ed5fee2ffb67a79e8c4e6dd3f5e7a
semernitskaya <semernitskaya@gmail.com>
2014-09-06 17:23:23 +0400

    base project structure

---
+++ src/main/java/org/ecwid/ApplicationStarter.java
@@ -0,0 +1,11 @@
+package org.ecwid;
+
+/**
+ * Author: Semernitskaya
+ */
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        System.out.println("fghfdkjgkdjg");
+    }
+}
